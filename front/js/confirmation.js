b9b05472e5ca491aac853f043326f8dd9b4dcdd5
LionelVanS <lionelvs@outlook.fr>
2022-08-06 19:21:03 +0200

    Suppression du localStorage et affichage du numéro de commande

---
+++ front/js/confirmation.js
@@ -0,0 +1,15 @@
+// Suppression du localStorage
+window.localStorage.clear()
+
+getOrderId()
+
+// Récupération de l'orderId via les parametres d'url
+function getOrderId(){
+    const currentUrl = window.location.search
+    const orderIdFromUrl = new URLSearchParams(currentUrl)
+    for(let params of orderIdFromUrl){
+        id = params[1]
+    }
+    const orderId = document.querySelector("#orderId")
+    orderId.textContent = id
+}
