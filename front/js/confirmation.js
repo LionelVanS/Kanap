549e74cfae2e2c393d56d704126203a4178b0aa4
LionelVanS <lionelvs@outlook.fr>
2022-08-12 14:29:24 +0200

    Refactorisation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,8 +1,6 @@
 // Suppression du localStorage
 window.localStorage.clear()
 
-getOrderId()
-
 // Récupération de l'orderId via les parametres d'url
 function getOrderId(){
     const currentUrl = window.location.search
@@ -11,7 +9,13 @@ function getOrderId(){
     for(let params of orderIdFromUrl){
         id = params[1]
     }
+    
+    showOrderId(id)
+}
 
+function showOrderId(id){
     const orderId = document.querySelector("#orderId")
     orderId.textContent = id
 }
+
+getOrderId()
